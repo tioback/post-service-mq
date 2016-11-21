edc664d2f3f559dec6af3fdf94cf67cdf442b507
renato <renato@knewin.com>
2016-11-21 03:45:01 -0200

    ALteração endereço servidor AMQP.

--- src/main/java/br/ufsc/grad/renatoback/tcc/post/service/mq/PostServiceMqApplication.java
+++ src/main/java/br/ufsc/grad/renatoback/tcc/post/service/mq/PostServiceMqApplication.java
@@ -28,7 +28,7 @@ public class PostServiceMqApplication {
 		URI ampqUrl;
 		try {
 			// ampqUrl = new URI(getEnvOrThrow("CLOUDAMQP_URL"));
-			ampqUrl = new URI("amqp://rmcokgtw:PL6Cs58f9fnWzcIb2uGed-aTx3G5cPaE@buck.rmq.cloudamqp.com/rmcokgtw");
+			ampqUrl = new URI("amqp://flifteha:jRG6EbAeuvSbGLl2aHPk79tYc_fbpxab@buck.rmq.cloudamqp.com/flifteha");
 		} catch (URISyntaxException e) {
 			throw new RuntimeException(e);
 		}
